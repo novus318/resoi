f05f4375cc6fa8dcd485717c0a674936bb9e1416
novus318 <nizamudheen318@gmail.com>
2024-10-17 21:35:57 +0530

    changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <header className="w-full h-auto">
+    <header className="w-full h-autopt-10">
     <nav className="w-full top-0 shadow z-40 fixed bg-white  lg:px-10 md:px-7 px-2 flex justify-between transition-all pt-3">
       <div className="">
         <Link href={"/"}>
@@ -14,9 +14,6 @@ const Navbar = () => {
           />
         </Link>
       </div>
-
-
-    
     </nav>
   </header>
   )
